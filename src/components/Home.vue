d0a96acaa43325d7e9519fcf94271f8bf84d15db
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2024-01-17 16:38:58 +0500

    vue js first component

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>hello home components</h1>
+        <h1>{{name}}</h1>
+        <h1>{{lastName}}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'Home',
+    props:{
+        name:String,
+        lastName:String
+    }
+}
+</script>
