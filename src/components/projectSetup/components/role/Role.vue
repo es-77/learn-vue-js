61c1c8411ece91ed41e49bc1ddd244a0732738d4
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2024-02-02 18:16:51 +0500

    role add toaster message

--- src/components/projectSetup/components/role/Role.vue
+++ src/components/projectSetup/components/role/Role.vue
@@ -5,6 +5,7 @@
     <ErrorMessage name="name" /><br />
     <button type="submit">submit</button>
   </Form>
+  <ToastMessageVue :message="toastMessage" :variant="toastVariant" ref="toast" />
 </template>
 
 <script>
@@ -12,6 +13,7 @@ import { RoleValidation } from './RoleValidation';
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import axios from 'axios';
 import BackEndApiRoutes from '../../BackEndApiRoutes';
+import ToastMessageVue from '../../common/ToastMessage.vue';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -19,19 +21,26 @@ export default {
   components: {
     Form,
     Field,
-    ErrorMessage
+    ErrorMessage,
+    ToastMessageVue
   },
   data() {
     return {
-      schema: RoleValidation
+      schema: RoleValidation,
+      toastMessage: '',
+      toastVariant: 'info'
     };
   },
   methods: {
     async onSubmit(value) {
-      const response = await axios
-        .post(BackEndApiRoutes.roles.role_save, value)
-        .catch(error => console.error('Search Error:', error));
-      response?.data?.results ?? null;
+      const response = await axios.post(BackEndApiRoutes.roles.role_save, value).catch(error => {
+        (this.toastMessage = 'Not Created Role'), (this.toastVariant = 'error'), console.log(error);
+      });
+      if (response?.data) {
+        this.toastMessage = 'Role Created Successfuly';
+        this.toastVariant = 'success';
+      }
+      this.$refs.toast.show();
     }
   }
 };
